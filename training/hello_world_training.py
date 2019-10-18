26f06b88515665c6464d4649a13f987be4e793b1
Andrew <andrewscotthat@gmail.com>
2019-10-18 00:01:05 -0700

    Created kick off training.

--- training/hello_world_training.py
+++ training/hello_world_training.py
@@ -3,20 +3,22 @@ from pathlib import Path
 from rlbot.matchconfig.match_config import PlayerConfig, Team
 
 from bronze.bronze_exercises import bronze_exercises
-from stone.stone_exercises import stone_exercises
 from dribble.dribble_exercises import dribble_exercises
+from kickoff.kickoff_exercises import kickoff_exercises
+from stone.stone_exercises import stone_exercises
 
 
 def make_default_playlist():
     exercises = []
     # exercises += stone_exercises
     # exercises += bronze_exercises
-    exercises += dribble_exercises
+    # exercises += dribble_exercises
+    exercises += kickoff_exercises
 
     for exercise in exercises:
         exercise.match_config.player_configs = [
             PlayerConfig.bot_config(
-                Path(__file__).absolute().parent.parent / 'src' / 'main' / 'python' / 'botv1.cfg',
+                Path(__file__).absolute().parent.parent / 'src' / 'main' / 'python' / 'ut_version.cfg',
                 Team.BLUE)
         ]
 
---
+++ training/kickoff/kickoff_training.py
@@ -0,0 +1,46 @@
+from dataclasses import dataclass, field
+from math import pi
+
+from rlbot.utils.game_state_util import GameState, BoostState, BallState, CarState, Physics, Vector3, Rotator
+
+from rlbottraining.training_exercise import TrainingExercise
+from rlbottraining.common_graders.goal_grader import StrikerGrader
+from rlbottraining.grading.grader import Grader
+from rlbottraining.rng import SeededRandomNumberGenerator
+from rlbottraining.common_graders.tick_wrapper import GameTickPacketWrapperGrader
+from rlbottraining.training_exercise import Playlist
+
+@dataclass
+class KickOff(TrainingExercise):
+    grader: Grader = field(default_factory=lambda: GameTickPacketWrapperGrader(
+        StrikerGrader(timeout_seconds=5.0, ally_team=0)))
+    car_start_x: float = 0
+    car_start_y: float = -4608
+    car_yaw: float = 0
+
+    def make_game_state(self, rng: SeededRandomNumberGenerator) -> GameState:
+        return GameState(
+            ball=BallState(physics=Physics(
+                location=Vector3(0, 0, 100),
+                velocity=Vector3(0, 0, 0),
+                angular_velocity=Vector3(0, 0, 0))),
+            cars={
+                0: CarState(
+                    physics=Physics(
+                        location=Vector3(self.car_start_x, self.car_start_y, 0),
+                        rotation=Rotator(0, self.car_yaw, 0),
+                        velocity=Vector3(0, 0, 0),
+                        angular_velocity=Vector3(0, 0, 0)),
+                    boost_amount=33)
+            },
+            boosts={1: BoostState(100)},
+        )
+
+def make_default_playlist() -> Playlist:
+    return [
+        KickOff('Center Kickoff', car_start_x=0, car_start_y=-4608, car_yaw=(.5 * pi)),
+        KickOff('Left Kickoff', car_start_x=2048, car_start_y=-2560, car_yaw=(.75 * pi)),
+        KickOff('Left Center Kickoff', car_start_x=256, car_start_y=-3840, car_yaw=(.5 * pi)),
+        KickOff('Right Center Kickoff', car_start_x=-256, car_start_y=-3840, car_yaw=(.5 * pi)),
+        KickOff('Right Kickoff', car_start_x=-2048, car_start_y=-2560, car_yaw=(-.25 * pi)),
+    ]
