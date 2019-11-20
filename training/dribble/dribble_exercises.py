b96f444f5df1420eda1e7c040af34e968a3ec289
Andrew <andrewscotthat@gmail.com>
2019-11-20 05:47:27 -0800

    In flux

--- training/dribble/dribble_exercises.py
+++ training/dribble/dribble_exercises.py
@@ -3,4 +3,5 @@ from dribble.carry_training import StationaryBall
 dribble_exercises = [
     # StationaryBall('BallOnGround', ball_start_z=90, car_start_x=0, car_start_y=-1000),
     StationaryBall('Ball On Ground', ball_start_x=0, ball_start_y=-4000, ball_start_z=90, car_start_x=0, car_start_y=-4500),
+    # StationaryBall('Ball On Ground', ball_start_x=1000, ball_start_y=-4000, ball_start_z=90, car_start_x=1000, car_start_y=-4500),
 ]
